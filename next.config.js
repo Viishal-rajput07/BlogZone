e677b379d275ea25e58be6492748fb361efff333
Viishal-rajput07 <rajputanavishal7@gmail.com>
2023-08-31 16:04:35 +0530

    redirecting in next.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    redirects:async()=>{
+        return [
+            {
+                source: '/user',
+                destination: '/',
+                permanent: false
+            },
+            {
+                source: '/user/:userId',
+                destination: '/',
+                permanent: false
+            }
+        ]
+    } 
+        
+}
 
 module.exports = nextConfig
